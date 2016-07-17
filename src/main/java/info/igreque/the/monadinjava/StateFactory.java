72ec5979f6f25c66d93bda15182e5857feea41d4
Yuji Yamamoto <whosekiteneverfly@gmail.com>
2016-07-17 12:23:37 +0900

    Notes for the difference from the actual article

--- src/main/java/info/igreque/the/monadinjava/StateFactory.java
+++ src/main/java/info/igreque/the/monadinjava/StateFactory.java
@@ -1,6 +1,8 @@
 package info.igreque.the.monadinjava;
 
 class StateFactory<S> implements MonadFactory {
+  // 本当は↓のような型にしたい
+  // public <T> State<S, T> doNothingReturning(T value)
   public <T> Monad<T> doNothingReturning(T value){
     return new State<S, T>(
         (nonMutatedState) -> new MutationResult<>(nonMutatedState, value)
