2dc14b9668f7f620efab0d27265a1eca46940c5b
Yuji Yamamoto <whosekiteneverfly@gmail.com>
2016-07-17 16:06:16 +0900

    Copy comments from the source post

--- src/main/java/info/igreque/the/monadinjava/MutationResult.java
+++ src/main/java/info/igreque/the/monadinjava/MutationResult.java
@@ -2,8 +2,8 @@ package info.igreque.the.monadinjava;
 
 // 状態を書き換えた結果を表すValue Object
 class MutationResult<T, S> {
-  public final S newState;
-  public final T value;
+  public final S newState; // 新しい状態
+  public final T value;    // 一緒に返す値
 
   MutationResult(S newState, T value){
     this.newState = newState;
