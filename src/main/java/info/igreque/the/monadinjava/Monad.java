04b5666f0779424606aedff251a6d45290551fbc
Yuji Yamamoto <whosekiteneverfly@gmail.com>
2016-08-11 21:03:53 +0900

    Try to unify Monad file

--- src/main/java/info/igreque/the/monadinjava/Monad.java
+++ src/main/java/info/igreque/the/monadinjava/Monad.java
@@ -2,7 +2,14 @@ package info.igreque.the.monadinjava;
 
 import java.util.function.Function;
 
+// 本当は一つのインターフェースにまとめたいのですが、
+// Javaのインターフェースの都合上やむを得ず分けています。
+// 本来は2つ合わせて初めてMonadと呼べる点をお忘れなく。
 interface Monad<T1> {
-  // Haskellで言うところの >>= (bind)に該当します。
+
   <T2> Monad<T2> then(Function<T1, Monad<T2>> action);
+
+  interface Return {
+    <T> Monad<T> doNothingReturning(T value);
+  }
 }
